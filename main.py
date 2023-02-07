92df2c5dbd88e7ccebe9b3ab7b0a2d4e8b3abf31
Aditya Peddi <adityapeddi@gmail.com>
2023-02-06 19:12:41 -0700

    Add functions.py

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello World")
+fruits=["apples", "bananas", "oranges", "pineapple", "kiwi", "cherries", "tomato"]
+#
+# print(len(fruits))
+#
+# fruits[3]="lemon"
+#
+# print(fruits)
+#
+# fruits.append("lime")
+#
+# print(fruits)
+#
+# del fruits[0]
+# #
+# # print(fruits)
+#
+# fruits.remove("apples")
+#
+# print(fruits)
+#
+# # print(fruits[3])
+# fruits.append("water melon")
+#
+# print(fruits)
+#
+# print(fruits.pop())
+# print(fruits)
+
+# sorted_fruits= sorted(fruits)
+#
+# print(sorted_fruits)
+
+
+
+
+sorted_fruits= sorted(fruits,reverse=True)
+
+print(sorted_fruits)
